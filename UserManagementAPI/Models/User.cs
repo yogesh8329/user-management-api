141f8abf4144afaea0bfd806112cdbd4cc32dd82
Yogesh <ymrathod1412@gmail.com>
2026-03-02 16:02:04 +0530

    add user entity model

---
+++ UserManagementAPI/Models/User.cs
@@ -0,0 +1,18 @@
+namespace UserManagementAPI.Models;
+
+public class User
+{
+    public Guid Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string PasswordHash { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public bool IsActive { get; set; }
+}
